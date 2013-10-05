067a8b0c0ccf4dd408e129c7fbab962af860dbe2
Greg Martin <gregcmartin@gmail.com>
2013-10-05 00:12:33 +0000

    fixing django structure

--- CloudYara/CloudYara/cyapp/models.py
+++ CloudYara/CloudYara/cyapp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Rule(models.Model):
     name = models.CharField(max_length=200)
     rule = models.TextField()
+    description = models.CharField(max_length=200)
+
 
     
 
