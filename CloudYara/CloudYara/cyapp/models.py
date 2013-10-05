bbde9babc720484ac9dba1f5ef25e9f559c1c17a
Greg Martin <gregcmartin@gmail.com>
2013-10-04 17:37:20 -0700

    Update models.py

--- CloudYara/CloudYara/cyapp/models.py
+++ CloudYara/CloudYara/cyapp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+CLASSIFICATION_CHOICES = (
+    ('public', 'Public'),
+    ('private', 'Private'),
+)
+
 # Create your models here.
 class Rule(models.Model):
     name = models.CharField(max_length=200)
     rule = models.TextField()
+    source = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-
-
-    
-
-
+    reported_by_user = models.ForeignKey(User, blank=True, null=True)
+    classification = models.CharField(max_length=100, choices=CLASSIFICATION_CHOICES, blank=True, null=True)
+    version = models.CharField(max_length=20)
+    valid_rule = models.BooleanField()
